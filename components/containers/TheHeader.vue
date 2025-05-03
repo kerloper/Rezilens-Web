8ad171ed98db0ab40720ec0788dcdd2995b82607
kerloper <karimijavad990@gmail.com>
2025-05-03 09:19:55 +0400

    chore(global) : menu

--- components/containers/TheHeader.vue
+++ components/containers/TheHeader.vue
@@ -94,6 +94,7 @@
 
 <script setup>
 import MobileNav from "~/components/nav/MobileNav.vue";
+import {menuItems} from "~/constants/config";
 
 const header = ref(null)
 const isOpenMainNav = ref(false)
@@ -117,86 +118,6 @@ const closeSubmenus = () => {
 };
 
 const basketTabFlag = useCookie('basket_tab_flag')
-const menuItems = ref(
-    [
-      {
-        "title": "Home",
-        "description": "A high-level overview of our value proposition, customer success story, and quick links to core offerings.",
-        "target": "/"
-      },
-      {
-        "title": "About Us",
-        "target": "/about-us",
-        "subitems": [
-          {
-            "title": "Firm story & mission",
-            "target": "/about-us/firm-story"
-          },
-          {
-            "title": "Leadership team bios",
-            "target": "/about-us/leadership"
-          },
-          {
-            "title": "Core values & differentiators",
-            "target": "/about-us/diff"
-          }
-        ]
-      },
-      {
-        "title": "Services",
-        "target": "/services",
-        "subitems": [
-          {
-            "title": "Strategic Foresight",
-            "target": "/services/strategic"
-          },
-          {
-            "title": "Intelligent Solutions",
-            "target": "/services/intelligent"
-          },
-          {
-            "title": "Trust & Resilience",
-            "target": "/services/cybersecurity"
-          },
-          {
-            "title": "Adaptive Transformation",
-            "target": "/services/esg"
-          }
-        ]
-      },
-      {
-        "title": "Industries",
-        "target": "/industries"
-      },
-      {
-        "title": "Resources",
-        "target": "/resources",
-        "subitems": [
-          {
-            "title": "Blog posts",
-            "target": "/resources/post"
-          },
-          {
-            "title": "Reports",
-            "target": "/resources/report"
-          },
-          {
-            "title": "News & Events",
-            "target": "/resources/new"
-          }
-        ]
-      },
-      {
-        "title": "Careers",
-        "target": "/careers"
-      },
-      {
-        "title": "Contact",
-        "description": "Inquiry forms, office locations & direct-reach information.",
-        "target": "/contact-us"
-      }
-    ]
-)
 
 watch(() => basketTabFlag.value, (value) => {
   if (!showBasketTab.value) {
--- constants/config.js
+++ constants/config.js
@@ -1,2 +1,65 @@
-
-export const baseUrl = 'https://yadapi.kerloper.com/'
+export const baseUrl = ''
+export const menuItems = [
+    {
+        "title": "Home",
+        "description": "A high-level overview of our value proposition, customer success story, and quick links to core offerings.",
+        "target": "/"
+    },
+    {
+        "title": "About Us",
+        "target": "/about-us",
+    },
+    {
+        "title": "Services",
+        "target": "/services",
+        "subitems": [
+            {
+                "title": "Strategic Foresight",
+                "target": "/services/strategic"
+            },
+            {
+                "title": "Intelligent Solutions",
+                "target": "/services/intelligent"
+            },
+            {
+                "title": "Trust & Resilience",
+                "target": "/services/cybersecurity"
+            },
+            {
+                "title": "Adaptive Transformation",
+                "target": "/services/esg"
+            }
+        ]
+    },
+    {
+        "title": "Industries",
+        "target": "/industries"
+    },
+    {
+        "title": "Resources",
+        "target": "/resources",
+        "subitems": [
+            {
+                "title": "Blog posts",
+                "target": "/resources/post"
+            },
+            {
+                "title": "Reports",
+                "target": "/resources/report"
+            },
+            {
+                "title": "News & Events",
+                "target": "/resources/new"
+            }
+        ]
+    },
+    {
+        "title": "Careers",
+        "target": "/careers"
+    },
+    {
+        "title": "Contact",
+        "description": "Inquiry forms, office locations & direct-reach information.",
+        "target": "/contact-us"
+    }
+]
